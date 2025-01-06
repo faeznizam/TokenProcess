c8e4279637e648a74536ff204c430219af4e6c31
Muhammad Faez Mohammad Nizam <faeznizam@gmail.com>
2025-01-06 12:37:40 +0800

    update code

--- ui/ui_file.py
+++ ui/ui_file.py
@@ -1,21 +1,39 @@
 import sys
+import logging
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QComboBox, QLabel, QTextEdit, QPushButton, QFileDialog
 
-
-from logic.return_file_logic import return_file_process_flow
-from logic.send_file_logic import send_file_process_flow
+from logic.return_file_from_cybs import return_file_from_cybs_main
+from logic.send_file_to_cybs import to_send_files_to_cybs_main
 from logic.summary import summary_analysis
+from logic.import_iPay88_token_to_secondary_token import import_iPay88_token_to_secondary_token_main
+from logic.import_NCT_and_EG_to_secondary_token import import_NCT_EG_to_secondary_token_main
+from logic.import_iPay88_token_to_main_token import import_iPay88_token_to_main_token_main
+from logic.send_file_to_iPay88 import send_file_to_iPay88_main
 
+class QTextEditLogger(logging.Handler):
+    def __init__(self, text_widget):
+        super().__init__()
+        self.text_widget = text_widget
 
+    def emit(self, record):
+        # Format the log message
+        msg = self.format(record)
+        # Append the message to the QTextEdit
+        self.text_widget.append(msg)
 
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.selected_folder = None  # Initialize variable to store the selected folder path
         self.function_mapping = {
-            'Prepare File For CYB': self.process_cyb,
-            'Remap To SF': self.process_sf,
-            'Analysis': self.summary
+            'Send Files To Cybersource': self.process_cyb,
+            'Remap Cybersource Token To Original File': self.process_sf,
+            'Cybersource Token File Analysis': self.summary,
+            'To Import iPay88 Token To Secondary Token Field' : self.iPay88_to_secondary_token,
+            'To Import NCT and EG To Secondary Token Field' : self.NCT_and_EG_to_secondary_token,
+            'Send File To iPay88' : self.send_file_to_ipay88,
+            'Import iPay88 Token To Main Token Field' : self.import_iPay88_token_to_main_token
+
         }
 
         # Initialize UI
@@ -34,7 +52,12 @@ class MyApp(QWidget):
         self.browse_button.setEnabled(False)  # Disable the button initially
 
         # Add items to combo box
-        self.combo.addItems(['', 'Prepare File For CYB', 'Remap To SF', 'Analysis'])  # Blank option at the top
+        self.combo.addItems(
+            ['', 'Send Files To Cybersource', 'Remap Cybersource Token To Original File', 
+             'Cybersource Token File Analysis', 'To Import iPay88 Token To Secondary Token Field',
+             'To Import NCT and EG To Secondary Token Field',
+             'Send File To iPay88', 'Import iPay88 Token To Main Token Field'
+            ])  # Blank option at the top
 
         # Connect signal to slot
         self.combo.currentIndexChanged.connect(self.on_dropdown_select)
@@ -52,6 +75,18 @@ class MyApp(QWidget):
         self.setWindowTitle('CYB SF Module')
         self.setGeometry(100, 100, 400, 300)
 
+    def setup_logging(self):
+        """Set up logging to display messages in the QTextEdit."""
+        self.log_handler = QTextEditLogger(self.text_display)
+        self.log_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        self.log_handler.setFormatter(formatter)
+
+        # Add the custom handler to the root logger
+        root_logger = logging.getLogger()
+        root_logger.addHandler(self.log_handler)
+        root_logger.setLevel(logging.INFO)
+    
     def on_dropdown_select(self):
         selected_item = self.combo.currentText()
 
@@ -77,14 +112,14 @@ class MyApp(QWidget):
     def process_cyb(self, folder_path):
         # Processing logic for "Prepare File For CYB"
         
-        end_result = send_file_process_flow(folder_path)
+        end_result = to_send_files_to_cybs_main(folder_path)
         self.text_display.append(end_result)
         
 
     def process_sf(self, folder_path):
         # Processing logic for "Remap To SF"
         
-        end_result2 = return_file_process_flow(folder_path)
+        end_result2 = return_file_from_cybs_main(folder_path)
         self.text_display.append(end_result2)
 
     def summary(self, folder_path):
@@ -92,7 +127,25 @@ class MyApp(QWidget):
         
         end_result3 = summary_analysis(folder_path)
         self.text_display.append(end_result3)
-        
+
+    def iPay88_to_secondary_token(self, folder_path):
+        # processing logic for ipay88 to secondary token
+        end_result4 = import_iPay88_token_to_secondary_token_main(folder_path)
+        self.text_display.append(end_result4)
+
+    def NCT_and_EG_to_secondary_token(self, folder_path):
+
+        end_result5 = import_NCT_EG_to_secondary_token_main(folder_path)
+        self.text_display.append(end_result5)
+
+    def send_file_to_ipay88(self, folder_path):
+        end_result6 = send_file_to_iPay88_main(folder_path)
+        self.text_display.append(end_result6)
+
+    def import_iPay88_token_to_main_token(self, folder_path):
+        end_result7 = import_iPay88_token_to_main_token_main(folder_path)
+        self.text_display.append(end_result7)
+
         
 
 
