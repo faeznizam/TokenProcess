5b05a007ff9fd62f89c85b6af8340f5473067086
Muhammad Faez Mohammad Nizam <faeznizam@gmail.com>
2025-05-30 10:25:50 +0800

    Finalizing code for production. Adding logging, time module for calculating time lapsed, and integrate code with UI file.

--- logic/sw_process.py
+++ logic/sw_process.py
@@ -1,5 +1,7 @@
 """
-CODE PURPOSE: To convert original file to SG file format. 
+CODE PURPOSE: 
+1. To convert original file to SG file format. 
+2. To extract data from result file (.csv) and reformat in table form in .xlsx
 """
 
 # import module
@@ -9,6 +11,8 @@ import logging
 from openpyxl import load_workbook
 from openpyxl.styles import NamedStyle
 import re
+import time
+import numpy as np
 
 
 # logging setup
@@ -16,7 +20,9 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 
 
 # use vectorize dataframe instead of initalize empty dataframe method
-def convert_salesfile_to_sgformat(df):
+def generate_sg_format_df(df):
+    # generate sg format dataframe with salesfile data
+
     sg_df = pd.DataFrame({
         'ConstID': '',
         'CustomerID': '',
@@ -112,7 +118,7 @@ def convert_salesfile_to_sgformat(df):
         'optional_one': '',
         'optional_two': '',
         'optional_three': '',
-        'Payment Category': df['CARDTYPE'] + " " + df['DEBIT_CC_CARD'],
+        'Payment Category': df['CARDTYPE'],
         'PolicyNoDate': '',
         'InstantDebit': '',
         'PLEDGETYPE': 'Standard',
@@ -133,14 +139,21 @@ def convert_salesfile_to_sgformat(df):
 
     return sg_df
 
+def transform_payment_category(x):
+    
+    if x == 'MBB':
+        'MBB'
 
 def transform_card_type(x):
-    if x == 'DEBIT CARD':
-        return 'DC'
-    elif x == 'CREDIT CARD':
-        return 'CC'
-    else:
-        x
+    # using 
+    lookup = {
+        'debit card' : 'DC',
+        'credit card' : 'CC'
+    }
+
+    if isinstance(x, str):
+        return lookup.get(x.lower(), x)
+    return x
 
 
 def create_new_file_name(file):
@@ -149,39 +162,46 @@ def create_new_file_name(file):
 
 
 def convert_sw_file(folder_path, file):
-
+    logging.info('Process 1: Create file with SG format using SW file')
     file_path = os.path.join(folder_path, file)
 
-    logging.info('Read file')
+    logging.info(f'Read {file}')
     data = pd.read_excel(file_path, dtype={'EXPIRY' : str, 'ACCOUNT NUMBER' : str, 'POSTCODE' : str , 'TEL HP': str})
-
-    data = data.fillna('')
     
-    data['IC NUMBER'] = data['IC NUMBER'].where(data['IC NUMBER'] != '' , data['PASSPORT NUMBER'])
+    logging.debug('Fill blank ic number in IC NUMBER column with data from PASSPORT NUMBER column')
+    data['IC NUMBER'] = data['IC NUMBER'].fillna(data['PASSPORT NUMBER'])
 
-    logging.info('Convert card type')
+    logging.debug('Transform card type according to its classification')
     data['DEBIT_CC_CARD'] = data['DEBIT_CC_CARD'].apply(lambda x : transform_card_type(x))
 
-    logging.info('Make data in title format')
+    logging.debug('Transform data in CARDTYPE in title case')
     data['CARDTYPE'] = data['CARDTYPE'].str.title()
 
-    logging.info('Replace blank in CARDTYPE with MBB')
+    logging.debug('Replace blank in CARDTYPE with MBB marking and combine data with DEBIT_CC_CARD')
     data['CARDTYPE'] = data['CARDTYPE'].apply(lambda x: 'MBB' if pd.isna(x) or x == '' else x)
-    data['DEBIT_CC_CARD'] = data['DEBIT_CC_CARD'].fillna('')
+    data['CARDTYPE'] = np.where(data['CARDTYPE'] == 'MBB', data['CARDTYPE'], data['CARDTYPE'] + ' ' + data['DEBIT_CC_CARD'])
+    
+    logging.info('Create SG Format table with SW data')
+    new_df = generate_sg_format_df(data)
 
-    new_df = convert_salesfile_to_sgformat(data)
-
+    logging.debug('Reformat CreatedDate, D0, and DOB columns date to DD/MM/YYYY')
     new_df['CreatedDate'] = pd.to_datetime(new_df['CreatedDate'], format='%d/%m/%Y').dt.strftime('%d-%m-%Y')
     new_df['D0'] = pd.to_datetime(new_df['D0'], format='%d/%m/%Y').dt.strftime('%d-%m-%Y')
     new_df['DOB'] = pd.to_datetime(new_df['DOB'], format='%d/%m/%Y').dt.strftime('%d-%m-%Y')
+
+    logging.debug("Replace 'AMEX' with 'amex cc' in Payment Category column")
     new_df['Payment Category'] = new_df['Payment Category'].apply(lambda x: 'AMEX' if isinstance(x, str) and x.strip().lower() == 'amex cc' else x)
     
-    output_file_path = os.path.join(folder_path, create_new_file_name(file))
-    
-    logging.info('Saving file...')
-    new_df.to_excel(output_file_path, index=False)
+    logging.debug(f'Create new file name: {create_new_file_name(file)}')
+    file_name = create_new_file_name(file)
 
-    logging.info('Open saved file')
+    logging.debug('Create file path to save result')
+    output_file_path = os.path.join(folder_path, file_name)
+    
+    logging.info(f'Save {file_name} in {folder_path}')
+    new_df.to_excel(output_file_path, index=False)
+    
+    logging.info(f'Re-open {file_name} file.')
     wb = load_workbook(output_file_path)
     ws = wb.active
 
@@ -213,22 +233,30 @@ def convert_sw_file(folder_path, file):
                     cell = ws.cell(row=row_idx, column=column_index, value=formula)
                     cell.style = date_style
 
-    # Apply to both columns
+    logging.debug('Reformat date in SubDate and LatestCollectedDate using formula format')
     apply_date_formula(subdate_col_idx, new_df["SubDate"])
     apply_date_formula(latest_collected_col_idx, new_df["LatestCollectedDate"])
 
-    logging.info('Saving file')
+    logging.info(f'Save {file_name} in {folder_path}')
     wb.save(output_file_path)
 
-    logging.info('Process complete. File saved in folder.')
+    # table info
+    count_onetime = (new_df['Frequency'] == 0).sum()
+    count_pledge = (new_df['Frequency'] > 0).sum()
+
+    logging.info(f'Table length: {len(new_df)}, Total Pledge: {count_pledge}, Total One-Time: {count_onetime}')
+    logging.info(f'Process 1 completed')
+
     
 
-def filename_for_result_file(file):
+    
+    
+def extract_batchid_and_create_filename(file):
     match = re.search(r'\b\d{6}\b', file)
     return f'Extracted result from {match.group()}.xlsx'
 
 
-def extract_result_from_result_file(folder_path, file):
+def extract_data_from_result_file(folder_path, file):
     """
     What does this function do?
     1. Open .csv file from file_path given.
@@ -236,26 +264,32 @@ def extract_result_from_result_file(folder_path, file):
     3. Return as dataframe
     """
 
+    logging.info('Process 2: Extract data from EBC result file')
+    logging.debug('Create file path')
     file_path = os.path.join(folder_path, file)
 
     filename = file
-    
+
+    logging.debug('Create dictionary with desired key to parse')
     desired_keys = {'merchantReferenceCode',
                     'paySubscriptionCreateReply_subscriptionID',
                     'paySubscriptionCreateReply_instrumentIdentifierID'
                     }
 
+    logging.debug('Create list for parsed data')
     parsed_data = []
 
     
-
+    logging.info(f'Read file: {file}')
+    logging.info('Parsed data from result file and create dataframe table')
     with open(file_path, 'r') as file:
         lines = file.readlines()
         
-        # Skip the first two rows if necessary
+        # Skip the first two rows 
         if len(lines) > 2:
             lines = lines[2:]
 
+        logging.debug('Parsed data from file')
         for line in lines:
             line = line.strip()
             if line:  # Skip empty lines
@@ -270,15 +304,32 @@ def extract_result_from_result_file(folder_path, file):
                             parsed_dict[key] = value
                 parsed_data.append(parsed_dict)
     
-    # Convert the parsed data to a DataFrame manually if needed
+    logging.debug('Turn parsed data into table and save in variable')
     parsed_df = pd.DataFrame(parsed_data)
 
-    parsed_df.to_excel(os.path.join(folder_path, filename_for_result_file(filename)), index=False)
-    
+    logging.debug('Create new file name')
+    new_file_name = extract_batchid_and_create_filename(filename)
+
+    logging.info(f'Save {new_file_name} in {folder_path}')
+    parsed_df.to_excel(os.path.join(folder_path, new_file_name), index=False)
+
+    # table info
+    table_length = len(parsed_df)
+    token_count = len(parsed_df['paySubscriptionCreateReply_subscriptionID'])
+    failed_token_count = (
+        (parsed_df['paySubscriptionCreateReply_subscriptionID'] == '') | 
+        (parsed_df['paySubscriptionCreateReply_subscriptionID'].isna())
+        ).sum()
+
+    logging.info(f'Table length: {table_length}, Token Success: {token_count}, Token Failed: {failed_token_count} ')
+    logging.info('Process 2 completed')
     
 
-def main():
-    folder_path = r'C:\Users\mfmohammad\OneDrive - UNICEF\Documents\SW process\May\20250516'
+def process_sw_and_result_file(folder_path):
+    #folder_path = r'C:\Users\mfmohammad\OneDrive - UNICEF\Documents\2 Areas\Saleswork Process\May\20250529'
+    
+    logging.info('Process Starts!')
+    start_time = time.time() 
 
     directory = os.listdir(folder_path)
 
@@ -289,10 +340,12 @@ def main():
 
         elif 'unicef_malaysia' in file and 'reply.all' in file:
             
-            extract_result_from_result_file(folder_path, file)
+            extract_data_from_result_file(folder_path, file)
 
-            
+    elapsed_time = time.time() - start_time
+    logging.info(f'Process Completed. Elapsed time : {elapsed_time:.2f} seconds.')
 
-            
-if __name__ == "__main__":
-    main()
+"""       
+if __name__ == '__main__' :
+    process_sw_and_result_file()
+"""
